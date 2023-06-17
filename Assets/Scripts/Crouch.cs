7fd1715ab1f2bacda1f0ffe4dc443652c7a7a9fd
CamiloRoma <camiloa.romerom@utadeo.edu.co>
2023-06-17 15:55:15 -0500

    MainCharacter1

---
+++ Assets/Scripts/Crouch.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crouch : MonoBehaviour
+{
+   
+    public CharacterController PlayerHeight;
+    public float normalHeight, crouchHeight;
+
+   void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            PlayerHeight.height = crouchHeight;
+        }
+
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            PlayerHeight.height = normalHeight;
+        }
+    }
+}
