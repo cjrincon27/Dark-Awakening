36909c9685798c6ce8263c1c1efd1301f1c378a0
LauMora1es <lau.morales.ajc@gmail.com>
2023-06-17 23:13:53 -0500

    Update - Enemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public Transform player;
+    public const float chaseSpeed = 2f;
+    public float chaseRotationSpeed = 5f;
+    public float detectionRange = 150f;
+    public float chaseRange = 80f;
+
+    public float patrolSpeed = 3f;
+    public float patrolArrivalDistance = 0.2f;
+
+    private int currentWaypointIndex = 0;
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if (distance <= detectionRange && distance <= chaseRange)
+        {
+            ChasePlayer(); // Si el jugador esta dentro del rango lo persigue
+        }
+        else
+        {
+            Patrol(); // Si el jugador está fuera de rango, el enemigo patrulla
+        }
+    }
+
+    private void ChasePlayer()
+    {
+
+        Vector3 direction = (player.position - transform.position).normalized;
+        transform.position += direction * chaseSpeed * Time.deltaTime;
+
+        // Rota hacia el jugador
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, chaseRotationSpeed * Time.deltaTime);
+    }
+
+
+    private void Patrol()
+    {
+        Vector3 direction = (waypoints[currentWaypointIndex].position - transform.position).normalized;
+        transform.position += direction * patrolSpeed * Time.deltaTime;
+
+        // Comprueba si el enemigo ha llegado al waypoint actual
+        float distanceToWaypoint = Vector3.Distance(transform.position, waypoints[currentWaypointIndex].position);
+        if (distanceToWaypoint <= patrolArrivalDistance)
+        {
+            currentWaypointIndex++;
+
+            // Si el enemigo llega al final de la lista, volvemos al primer waypoint
+            if (currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+    }
+}
