d690e24b0ba85f5511e4751568ffbbcd48fb602c
cjrincon27 <boloncahs@gmail.com>
2023-06-28 11:30:50 -0500

    otra vez xD

--- Assets/Scripts/raycasts.cs
+++ Assets/Scripts/raycasts.cs
@@ -1,21 +1,58 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class raycasts : MonoBehaviour
 {
     [SerializeField] private LayerMask whatToDetect;
-    // Update is called once per frame
+    public GameObject enemiesContainer;
+    private Transform[] enemyTransforms;
+    private Vector3[] originalPositions;
+
+    void Start()
+    {
+        int enemyCount = enemiesContainer.transform.childCount;
+        enemyTransforms = new Transform[enemyCount];
+        originalPositions = new Vector3[enemyCount];
+
+        for (int i = 0; i < enemyCount; i++)
+        {
+            enemyTransforms[i] = enemiesContainer.transform.GetChild(i);
+            originalPositions[i] = enemyTransforms[i].position;
+        }
+    }
+
     void Update()
     {
-        float maxDistance = 30f;
+        float maxDistance = 10f;
         RaycastHit hit;
-        Ray ray = new Ray(transform.position, transform.forward );
-        Debug.DrawRay(ray.origin, ray.direction * 30f, Color.green);
-        if (Physics.Raycast(ray, out hit,maxDistance,whatToDetect))
-        {
-            hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
-        }
 
+        Ray ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * 10f, Color.green);
+        if (Physics.Raycast(ray, out hit, maxDistance, whatToDetect))
+        {
+            Debug.Log(hit);
+
+            foreach (Transform enemyTransform in enemyTransforms)
+            {
+                if (hit.collider.gameObject == enemyTransform.gameObject)
+                {
+                    StartCoroutine(DisappearAndAppear(enemyTransform));
+                }
+            }
+        }
+    }
+
+    private IEnumerator DisappearAndAppear(Transform enemyTransform)
+    {
+        // Desactivar el enemigo
+        enemyTransform.gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(5f);
+
+        // Activar el enemigo y restablecer su posici�n original
+        enemyTransform.gameObject.SetActive(true);
+        int index = System.Array.IndexOf(enemyTransforms, enemyTransform);
+        enemyTransform.position = originalPositions[index];
     }
 }
