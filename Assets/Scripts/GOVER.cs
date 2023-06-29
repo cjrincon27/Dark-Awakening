e36b97c4cc21bd1545b9c0e2b80b56d44e727a4d
CamiloRoma <camiloroma26@gmail.com>
2023-06-29 13:15:17 -0500

    mascosas

--- Assets/Scripts/GOVER.cs
+++ Assets/Scripts/GOVER.cs
@@ -5,17 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class GOVER : MonoBehaviour
 {
-    private void Start()
-    {
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             SceneManager.LoadScene("GAMEOVER"); 
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
         }
+        
     }
 
 }
