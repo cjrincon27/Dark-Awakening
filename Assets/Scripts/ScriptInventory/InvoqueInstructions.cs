3326dc3c6b6550d9364b5348ec63607668427237
PameMontes94 <132422090+PameMontes94@users.noreply.github.com>
2023-06-29 20:01:44 -0500

    UI pauseCorrect

--- Assets/Scripts/ScriptInventory/InvoqueInstructions.cs
+++ Assets/Scripts/ScriptInventory/InvoqueInstructions.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.UI;
+using UnityEngine.UI;
+using TMPro;
 
 public class InvoqueInstructions : MonoBehaviour
 {
-    //public Canvas canvasInstructions;
+    public Canvas canvasInstructions;
     public Canvas pauseCanvas;
     private bool canvasPause;
-    //private bool canvasInstructionsActive;
+    private bool canvasInstructionsActive;
+    
+   
 
     // Start is called before the first frame update
     void Start()
     {
-        //canvasInstructions.gameObject.SetActive(false);
+        canvasInstructions.gameObject.SetActive(false);
         pauseCanvas.gameObject.SetActive(false);
 
         Cursor.lockState = CursorLockMode.None;
@@ -25,41 +28,35 @@ public class InvoqueInstructions : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            //if (canvasInstructionsActive)
+            if (canvasInstructionsActive)
             {
-               // canvasInstructions.gameObject.SetActive(false);
-               // canvasInstructionsActive = false;
+                canvasInstructions.gameObject.SetActive(false);
+                canvasInstructionsActive = false;
             }
-            //else
+            else
             {
                 canvasPause = !canvasPause;
             }
         }
 
-        //if (canvasInstructionsActive)
-        
-            //pauseCanvas.gameObject.SetActive(false);
-            //Time.timeScale = 0f;
-        
-        else
-        {
-            if (canvasPause)
-            {
-                pauseCanvas.gameObject.SetActive(true);
-                Time.timeScale = 0f;
-            }
-            else
-            {
-                pauseCanvas.gameObject.SetActive(false);
-                Time.timeScale = 1.0f;
-            }
-        }
+        pauseCanvas.gameObject.SetActive(canvasPause && !canvasInstructionsActive);
+        Time.timeScale = (canvasPause || canvasInstructionsActive) ? 0f : 1.0f;
+    
     }
-    //public void ActivarCanvasInstructionsOnClick()
-    
-        //canvasInstructions.gameObject.SetActive(true);
-        //canvasInstructionsActive = true;
-        
-        
-    
+
+    public void ActivarCanvasInstructionsOnClick()
+    {
+        canvasInstructions.gameObject.SetActive(true);
+        canvasInstructionsActive = true;
+
+        pauseCanvas.gameObject.SetActive(false);
+      
 }
+   
+}
+
+
+
+
+
+
