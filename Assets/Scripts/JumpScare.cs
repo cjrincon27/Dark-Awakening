39c87b1f3ec2186d0952f5cceeeac7e4555a8b8f
CamiloRoma <camiloroma26@gmail.com>
2023-06-30 19:40:52 -0500

    New things

---
+++ Assets/Scripts/JumpScare.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScare : MonoBehaviour
+{
+    public GameObject JumpScareImg;
+    public AudioSource audioSource;
+
+    private bool isPlayed = false;
+
+    void Start()
+    {
+       JumpScareImg.SetActive(false); // DEactive the image in the gameobject in this case the jumpscare image
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (!isPlayed)
+            {
+                JumpScareImg.SetActive(true);
+                audioSource.Play();
+                StartCoroutine(DisableImg());
+
+                isPlayed = true;
+            }
+            
+        }
+    }
+
+    IEnumerator DisableImg()
+    {
+        yield return new WaitForSeconds(2);
+        JumpScareImg.SetActive(false);
+    }
+}
--- Assets/Scripts/ScriptInventory/Inventory.cs
+++ Assets/Scripts/ScriptInventory/Inventory.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 
@@ -13,6 +14,7 @@ public class Inventory : MonoBehaviour
     
 
     private GameObject[] slot;
+    
 
     public GameObject slotHolder;
 
