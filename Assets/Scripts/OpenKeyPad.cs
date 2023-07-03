fb833fd58baa0f1bb417d23d0192d72ea71b2445
CamiloRoma <camiloroma26@gmail.com>
2023-07-03 04:14:52 -0500

    kaypad(updated)

---
+++ Assets/Scripts/OpenKeyPad.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenKeyPad : MonoBehaviour
+{
+    public GameObject keypadOB;
+    public GameObject keypadText;
+
+    public bool inReach;
+
+
+    void Start()
+    {
+        inReach = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "MainCamera")
+        {
+            inReach = true;
+            keypadText.SetActive(true);
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "MainCamera")
+        {
+            inReach = false;
+            keypadText.SetActive(false);
+
+        }
+    }
+
+
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && inReach)
+        {
+            keypadOB.SetActive(true);
+        }
+        
+
+    }
+}
