666a05c711759bb98ae71ccb7f64925077964dd7
cjrincon27 <boloncahs@gmail.com>
2023-07-03 13:51:53 -0500

    linterna 4  usos

--- Assets/Scripts/FlashLight.cs
+++ Assets/Scripts/FlashLight.cs
@@ -8,66 +8,77 @@ public class FlashLight : MonoBehaviour
     public GameObject purplelight;
     public bool toggle;
     public AudioSource toggleSound;
+    public AudioSource dischargeSound;
+    private int purpleLightCount;
+    private bool isPurpleLightAvailable;
 
     void Start()
     {
-        if(toggle == false)
-        {
-            light.SetActive(false);
-            purplelight.SetActive(false);
-        }
-        if (toggle == true)
-        {
-            light.SetActive(true);
-            purplelight.SetActive(false);
-        }
-        
-        if(toggle == false)
-        {
-            purplelight.SetActive(false);
-            light.SetActive(false);
-        }
-        if (toggle == true)
-        {
-            purplelight.SetActive(true);
-            light.SetActive(false);
-        }
+        purpleLightCount = 0;
+        isPurpleLightAvailable = true;
+
+        light.SetActive(false);
+        purplelight.SetActive(false);
     }
-    
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
-            
         {
-            toggle = !toggle;
-            toggleSound.Play();
-            if(toggle == false)
+            ToggleLight();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (isPurpleLightAvailable && purpleLightCount < 4)
             {
-                light.SetActive(false);
-                purplelight.SetActive(false);
+                TogglePurpleLight();
             }
-            if (toggle == true)
+            else
             {
-                light.SetActive(true);
-                purplelight.SetActive(false);
+                dischargeSound.Play();
             }
         }
-        
-        if (Input.GetMouseButtonDown(1))
-            
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy") && purplelight.activeSelf)
         {
-            toggle = !toggle;
-            toggleSound.Play();
-            if(toggle == false)
-            {
-                purplelight.SetActive(false);
-                light.SetActive(false);
-            }
-            if (toggle == true)
-            {
-                purplelight.SetActive(true);
-                light.SetActive(false);
-            }
+            TogglePurpleLight();
+            ToggleLight();
+        }
+    }
+
+    void ToggleLight()
+    {
+        toggle = !toggle;
+        toggleSound.Play();
+
+        if (toggle == false)
+        {
+            light.SetActive(false);
+            purplelight.SetActive(false);
+        }
+        else if (toggle == true && !purplelight.activeSelf)
+        {
+            light.SetActive(true);
+        }
+    }
+
+    void TogglePurpleLight()
+    {
+        toggle = !toggle;
+        toggleSound.Play();
+
+        if (toggle == false)
+        {
+            purplelight.SetActive(false);
+        }
+        else if (toggle == true)
+        {
+            purplelight.SetActive(true);
+            purpleLightCount++;
         }
     }
 }
