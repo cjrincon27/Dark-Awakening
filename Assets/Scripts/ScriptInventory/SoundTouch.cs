a1af1ccc5bde23f95d5e04842974e58ee5d5e626
PameMontes94 <132422090+PameMontes94@users.noreply.github.com>
2023-07-03 17:31:10 -0500

    SoundItems

---
+++ Assets/Scripts/ScriptInventory/SoundTouch.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTouch : MonoBehaviour
+{
+    
+    [SerializeField] AudioClip clip; //pame
+
+    public float volume = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other) 
+
+    {
+        AudioSource.PlayClipAtPoint(clip, transform.position);
+        
+
+    }
+}
+
+
