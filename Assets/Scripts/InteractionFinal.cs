3477942488affa593ace375d7c50c4c6fa5d77df
PameMontes94 <132422090+PameMontes94@users.noreply.github.com>
2023-07-04 16:26:46 -0500

    ConexionCinematicaFinal

--- Assets/Scripts/InteractionFinal.cs
+++ Assets/Scripts/InteractionFinal.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InteractionFinal : MonoBehaviour
 {
     public GameObject gameObjectToActive;
     public GameObject intText;
     public bool interactable, toggle;
-    public AudioSource toggleSound;
+
+    private bool activateCinematic = false; //pame
+
     void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("MainCamera"))
@@ -24,30 +27,44 @@ public class InteractionFinal : MonoBehaviour
             intText.SetActive(false);
             interactable = false;
         }
-        
+
     }
 
     void Update()
-    { 
+    {
         if (interactable == true)
-      
+
         {
-           
-         if (Input.GetKeyDown(KeyCode.E))
-             
+
+            if (Input.GetKeyDown(KeyCode.E))
+
             {
                 toggle = !toggle;
-                toggleSound.Play();
-                
                 if (toggle == true)
                 {
                     gameObjectToActive.SetActive(true);
+                    StartCoroutine(ActivateCinematicAfterDelay(5f)); //pame
                 }
-                
+
                 intText.SetActive(false);
-                interactable = false; 
+                interactable = false;
             }
-        
+
+        }
+    }
+    IEnumerator ActivateCinematicAfterDelay(float delay) //pame
+    {
+        yield return new WaitForSeconds(delay);
+        activateCinematic = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (activateCinematic)
+        {
+            activateCinematic = false;
+            SceneManager.LoadScene(6);
         }
     }
 }
+
